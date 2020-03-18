d694d2668ae20c2c6685251588d67d3afab38d6d
Craig Stroberg <strobergcd@gmail.com>
2020-03-18 22:37:08 +0200

    Fish Added Extra Tests

--- src/test/java/com/craigsdel/FishTest.java
+++ src/test/java/com/craigsdel/FishTest.java
@@ -17,14 +17,14 @@ public class FishTest {
     }
 
     @Test
-    public void whenGivenFiveFishAndFirstOneIsMovingRightAndTheRestAreSmaller() {
+    public void whenGivenFiveFishAndTwoAreMovingRightAndTheRestAreSmaller() {
         List<Integer> fishSize = List.of(4, 8, 2, 6, 7);
         List<Integer> fishDirection = List.of(0, 1, 1, 0, 0);
         assertEquals(2, fish.howManyFishSurvive(fishSize, fishDirection));
     }
 
     @Test
-    public void whenGivenFiveFish() {
+    public void whenGivenFiveFishAndTheSecondIsMovingTowardsTheLargest() {
         List<Integer> fishSize = List.of(4, 3, 2, 1, 5);
         List<Integer> fishDirection = List.of(0, 1, 0, 0, 0);
         assertEquals(2, fish.howManyFishSurvive(fishSize, fishDirection));
